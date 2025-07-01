42913e8faba2f3ff35b7a525fcdae6c83cad29d6
balaji-atturu <balaji.atturu@gmail.com>
2025-07-01 12:57:05 +0530

    Added Dependencies

--- main-app/src/main.jsx
+++ main-app/src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../src/components/Styles.css';
+//import '../src/components/Styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
